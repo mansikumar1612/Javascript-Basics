1aebdaf8dc7314460522d938685247d6b4d2660e
mansikumar1612@gmail.com <mansikumar1612@gmail.com>
2021-09-02 19:29:04 +0530

    basics

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("hey this is my first piece of js code!! yay")
+
+console.log(123456789)
+
+console.log({name : "mansi"})
+
+console.log(true)
+
+console.log(false)
+
+console.log(false,23456,'string',{name : "alfred"})
+
+console.warn("This is a warning")
+
+console.error("This is error")
