d1e250c25c309c542ed381c00901dbe9386a5fca
mansikumar1612@gmail.com <mansikumar1612@gmail.com>
2021-09-02 20:15:13 +0530

    array operation

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-console.log("hey this is my first piece of js code!! yay")
+//Data types
+//primitive or non-primitive
 
-console.log(123456789)
+//variable
+ var name="mansi"
 
-console.log({name : "mansi"})
+ //primitive data types
+ let age =21
+ console.log(typeof age)
+ let Name="asds112234556"//whatever you write in it either it is number,alphabet,special character all of them together even it shows all are strings
+ console.log(typeof Name)
+ let isTrue = false //boolean
+ let brand
+ console.log(brand)
 
-console.log(true)
+ //non-primitive data types
 
-console.log(false)
-
-console.log(false,23456,'string',{name : "alfred"})
-
-console.warn("This is a warning")
-
-console.error("This is error")
+ let arr = [1,2,3,4,5]
+ console.log(typeof arr)
+ arr.push("value")
+ arr.push(false)
+ console.log(arr)
