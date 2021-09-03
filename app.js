933236704eb1aba739ab8ad4ec4b656b91d9f6f8
mansikumar1612@gmail.com <mansikumar1612@gmail.com>
2021-09-03 20:04:38 +0530

    comparison operators

--- app.js
+++ app.js
@@ -14,6 +14,32 @@ console.log(a-b)*/
 
 // assignment operators
 
-let name="mansi"
+/*let name="mansi"
 let a= 19
-console.log(a+name)
+console.log(a+name)*/
+
+//Comparison operators
+
+const a=21
+const b=7
+const c=21
+
+console.log(a>b)
+console.log(a<b)
+console.log(a==b)
+console.log(a==c)
+
+console.log("------------------------")
+
+const e=11
+const d='11'
+
+console.log(e==d)//string
+
+const f="gaurang"
+const g="Gaurang"
+
+console.log(f==g)//caps and small are different thing
+
+//===// first perform == and then compare the types
+
