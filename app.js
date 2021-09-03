328858684d7372b24c53308f249e71742823206b
mansikumar1612@gmail.com <mansikumar1612@gmail.com>
2021-09-03 19:37:13 +0530

    arithmetic operators

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-//Data types
-//primitive or non-primitive
+//operators
+//there are so many types of operators
+// last value will be print
+//1) Arithmetic operators
 
-//variable
- var name="mansi"
 
- //primitive data types
- let age =21
- console.log(typeof age)
- let Name="asds112234556"//whatever you write in it either it is number,alphabet,special character all of them together even it shows all are strings
- console.log(typeof Name)
- let isTrue = false //boolean
- let brand
- console.log(brand)
 
- //non-primitive data types
+const a=2
+const b=15
+let c=5
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a**b)// ** exponents
+console.log(b%a)
+ 
+//Assignment operators
 
- let arr = [1,2,3,4,5]
- console.log(typeof arr)
- arr.push("value")
- arr.push(false)
- console.log(arr)
+
+//c+=1// value of c=0 //instead of this we can also use c++
+//c = c+1 => 0+1 => 1
+ // multiplication operator using shorthand
+c**=3
+// c=c*5 => 5*5 => 25
+console.log(c)
+
