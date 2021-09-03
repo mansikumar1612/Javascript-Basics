a37cb6db6872d3bb22572adf4261fb3a6be4a24f
mansikumar1612@gmail.com <mansikumar1612@gmail.com>
2021-09-03 20:18:49 +0530

    Logical operators

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //operators
 //there are so many types of operators
 // last value will be print
+
 //1) Arithmetic operators
 
 /*const a="one"
@@ -12,7 +13,7 @@ const b=2
 console.log(a+b)
 console.log(a-b)*/
 
-// assignment operators
+// Assignment operators
 
 /*let name="mansi"
 let a= 19
@@ -20,7 +21,7 @@ console.log(a+name)*/
 
 //Comparison operators
 
-const a=21
+/*const a=21
 const b=7
 const c=21
 
@@ -37,9 +38,39 @@ const d='11'
 console.log(e==d)//string
 
 const f="gaurang"
-const g="Gaurang"
+const g="Gaurang"*/
 
-console.log(f==g)//caps and small are different thing
+//console.log(f==g)//caps and small are different thing
 
-//===// first perform == and then compare the types
+//===: first perform == and then compare the types
 
+//Logical operators
+
+//&&,||,!
+
+const a=2
+const b=3
+const c=35
+
+console.log(c>a && a<b)
+console.log(c>a && c<b)
+
+//console.log('a==b', a==b)
+//console.log('a!=b', a!=b)
+
+//console.log(" ")
+//console.log("truth table for AND")
+//console.log(" ")
+
+//console.log('false && false', false&&false)
+//console.log('false && true' ,false&&true)
+//console.log('true && false' ,true&&false)
+//console.log('true && true' ,true&&true)
+
+//console.log(" ")
+//console.log("truth table for OR")
+//console.log(" ")
+//console.log('false || false', false||false)
+//console.log('false || true', false||true)
+//console.log('true  || false', true||false)
+//console.log('true  || true', true||true)
