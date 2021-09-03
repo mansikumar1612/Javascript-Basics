de1b318c680429a4d60a38fd1b4d12ed7947e75e
mansikumar1612@gmail.com <mansikumar1612@gmail.com>
2021-09-03 20:43:43 +0530

    conditional operators

--- app.js
+++ app.js
@@ -48,12 +48,12 @@ const g="Gaurang"*/
 
 //&&,||,!
 
-const a=2
-const b=3
-const c=35
+//const a=2
+//const b=3
+//const c=35
 
-console.log(c>a && a<b)
-console.log(c>a && c<b)
+//console.log(c>a && a<b)
+//console.log(c>a && c<b)
 
 //console.log('a==b', a==b)
 //console.log('a!=b', a!=b)
@@ -74,3 +74,32 @@ console.log(c>a && c<b)
 //console.log('false || true', false||true)
 //console.log('true  || false', true||false)
 //console.log('true  || true', true||true)
+
+// conditional statements
+//if else elseif
+
+// check whether a number even or odd
+
+// if even do this
+// if odd do that
+
+const a= 0
+
+if(a%2 === 0)
+  {
+    console.log("number is even")
+  }
+else{
+  console.log("number is odd")
+}
+
+if(typeof a ==='number'){
+ if(a%2===0){
+    console.log("number is even")
+ }else {
+   console.log("number is odd")
+ }
+}
+else {
+  console.log("not a number")
+}
