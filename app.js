6ae98953967144ee31adcff32c8d33592f8c6a31
mansikumar1612@gmail.com <mansikumar1612@gmail.com>
2021-09-04 19:31:04 +0530

    node methods

--- app.js
+++ app.js
@@ -3,9 +3,28 @@
 
 //const element = document.getElementsByClassName('heading')
 
-//const element = document.getElementsByTagName('h2')
+//const element = document.getElementsByTagName('div')
 
 const element = document.querySelector('h3.heading') //perform all of these three
-console.log(element)
+//console.log(element)
 //query selector return only one element
-//for selecting all yiu havr to use queryselectorall
+//for selecting all you have to use queryselectorall
+
+//methods
+//element.innerHTML = "This is from JS"
+
+//element.style.color="blue"
+//element.style.fontsize="40px"
+//element.style="display:none"
+
+//console.log(element.classList)
+//element.classList.add('five')
+//console.log(element.classList)
+//element.classList.remove('one')
+//console.log(element.classList)
+
+//const body = document.querySelector('body')
+//console.log(body.classList.toggle('dark'))
+
+const value = element.classList.contains('three')
+console.log(value)
