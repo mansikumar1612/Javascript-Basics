0fbdbcf3fd2465b0865ecedeed62ca5e417dc0bb
mansikumar1612@gmail.com <mansikumar1612@gmail.com>
2021-09-06 18:46:35 +0530

    ES-6 functions

--- app.js
+++ app.js
@@ -14,17 +14,49 @@ function logName(Name="John Doe" , age = '21') {
   //Any variable you define inside a function's body is local to that function
 }
 //console.log(Name)
-function add(a=0,b=0)
-{
+//function add(a=0,b=0)
+//{
   //const sum = a+b
   //return sum
-  return a + b
-}
+  //return a + b
+//}
 //console.log(sum)
 //add(12,99)
 //const result = add(12,99)
 //console.log(result)
-const result = add()
-console.log(result)
+//const result = add()
+
+//console.log(result)
+
+//ECMA Script - Standard on which the javascript is based or viceversa.
+//ECMA's versions - ES-6,ES
+
+//another way of defining function
+
+//const add = function(a=0,b=0) {
+  //return a+b
+//}
+
+//console.log(add(12,33))
+
+//.......ARROW SYNTAX.......
+
+//const add = (a=0,b=0) => {
+  //return a+b
+//}
+
+//console.log(add(12,33))
+
+console.log(add(12,33))
+
+function add(a=0,b=0) {
+  return a+b
+}
+//ES-6 functions
+
+
+
+
+
 
 
