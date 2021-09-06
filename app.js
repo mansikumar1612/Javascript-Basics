9cb0cebaa4ba369fc29e862414a3572052db95af
mansikumar1612@gmail.com <mansikumar1612@gmail.com>
2021-09-06 19:32:48 +0530

    rock-paper-scissor

--- app.js
+++ app.js
@@ -1,62 +1,30 @@
-//const rock = document.querySelector('#rock')
-//const paper = document.querySelector('#paper')
-//const scissor = document.querySelector('#scissor')
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissor = document.querySelector('#scissor')
+const board = document.querySelector("#board")
 
-//function
-//var Name = 'mansi'
-function logName(Name="John Doe" , age = '21') {
-  //console.log('Mansi')
-  console.log(Name + " your age " + age)
 
-  //Never Misspell the function keyword
-  //A function can have multiple parameter
-  //You can set default values for the parameters
-  //Any variable you define inside a function's body is local to that function
+const handler = (e) => {
+    const action = e.target.id
+
+    switch (action) {
+        case "rock":
+            board.innerHTML = "Rock crushes scissors"
+            break
+        case 'paper':
+            board.innerHTML = "Paper traps Rock"
+            break
+        case 'scissor':
+            board.innerHTML = "Scissor cuts paper"
+            break
+        default:
+            board.innerHTML = "Click something"
+    }
+
 }
-//console.log(Name)
-//function add(a=0,b=0)
-//{
-  //const sum = a+b
-  //return sum
-  //return a + b
-//}
-//console.log(sum)
-//add(12,99)
-//const result = add(12,99)
-//console.log(result)
-//const result = add()
-
-//console.log(result)
-
-//ECMA Script - Standard on which the javascript is based or viceversa.
-//ECMA's versions - ES-6,ES
-
-//another way of defining function
-
-//const add = function(a=0,b=0) {
-  //return a+b
-//}
-
-//console.log(add(12,33))
-
-//.......ARROW SYNTAX.......
-
-//const add = (a=0,b=0) => {
-  //return a+b
-//}
-
-//console.log(add(12,33))
-
-console.log(add(12,33))
-
-function add(a=0,b=0) {
-  return a+b
-}
-//ES-6 functions
 
 
 
-
-
-
-
+rock.addEventListener('click', handler)
+paper.addEventListener('click', handler)
+scissor.addEventListener('click', handler)
