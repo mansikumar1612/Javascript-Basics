26ee8f893baa68b5f8036a894205796dde5d89a7
mansikumar1612@gmail.com <mansikumar1612@gmail.com>
2021-09-06 18:20:48 +0530

    function keyword

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-//Eventlisteners
+//const rock = document.querySelector('#rock')
+//const paper = document.querySelector('#paper')
+//const scissor = document.querySelector('#scissor')
 
-const button = document.querySelector('button')
-const element = document.querySelector('#selector')
+//function
+//var Name = 'mansi'
+function logName(Name="John Doe" , age = '21') {
+  //console.log('Mansi')
+  console.log(Name + " your age " + age)
 
-//button.addEventListener('click', () => {
-  //  console.log('you clicked me')
-//})
+  //Never Misspell the function keyword
+  //A function can have multiple parameter
+  //You can set default values for the parameters
+  //Any variable you define inside a function's body is local to that function
+}
+//console.log(Name)
+function add(a=0,b=0)
+{
+  //const sum = a+b
+  //return sum
+  return a + b
+}
+//console.log(sum)
+//add(12,99)
+//const result = add(12,99)
+//console.log(result)
+const result = add()
+console.log(result)
 
-//button.addEventListener('dbclick', () => {
-  //  console.log('you double clicked me')
-//})
 
-button.addEventListener('click', () => {
-    element.style = "background:aqua"
-})
